c2ec22a16d524bbac7b81177ae6a8b65d0dc5d85
Julia656 <ovcharenkojulia05@gmail.com>
2025-01-15 12:22:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const str1 = prompt("Введіть перший рядок:"); 
+const str2 = prompt("Введіть другий рядок:"); 
+const str3 = prompt("Введіть третій рядок:");
+
+
+if (line1 && line2 && line3) {
+    console.log(`${line3} ${line1} ${line2}`);
+} else {
+    console.log("Будь ласка, введіть непорожні рядки.");
+}
